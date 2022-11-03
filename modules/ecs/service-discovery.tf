ceae9c3fa9357d1195d349a36ffd41195066c77e
Sasidhar S Challa <SasidharS.Challa@meijer.com>
2022-11-03 10:10:53 +0530

    Initial Commits

--- modules/ecs/service-discovery.tf
+++
@@ -1,7 +0,0 @@
-# Create a service discovery where ECS tasks will be registered
-# and will be able to talk to each other
-resource "aws_service_discovery_private_dns_namespace" "dns_namespace" {
-  name        = "${aws_ecs_cluster.ecs_cluster.name}.local"
-  description = "Service discovery for ECS cluster ${aws_ecs_cluster.ecs_cluster.name}"
-  vpc         = data.aws_vpc.main.id
-}
